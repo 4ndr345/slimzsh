57debb09bf37fa335bc5cd83ea2f88947557c318
Andreas Kührmann <andreas.kuehrmann@egym.de>
2016-08-21 02:33:06 +0200

    feat: use zsh-autosuggestions

--- slim.zsh
+++ slim.zsh
@@ -19,7 +19,9 @@ source $slim_path/completion.zsh
 source $slim_path/aliases.zsh
 source $slim_path/correction.zsh
 source $slim_path/stack.zsh
+source $slim_path/zsh-autosuggestions/zsh-autosuggestions.zsh
 source $slim_path/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+source $slim_path/exports.zsh
 
 if command -v fasd >/dev/null 2>&1; then
   eval "$(fasd --init zsh-hook zsh-ccomp zsh-ccomp-install zsh-wcomp zsh-wcomp-install posix-alias)"
